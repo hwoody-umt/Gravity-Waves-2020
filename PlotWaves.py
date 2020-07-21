6221b9008482e3cecf917c1797ed1b59cf6d1df8
Keaton Blair <keatonblair3@gmail.com>
2020-07-21 09:43:15 -0600

    Changed title for plotting code

--- PlotWaves.py
+++ PlotWaves.py
@@ -2,60 +2,22 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import json
-from numpy.core.defchararray import lower  # For some reason I had to import this separately
 import matplotlib.dates as mdates
 import datetime
 import matplotlib.lines as mlines
-import matplotlib.markers as mmarkers
+from WaveDetectionFunctions import getUserInputFile
 
-
-def getUserInputFile(prompt):
-    print(prompt)
-    userInput = ""
-    while not userInput:
-        userInput = input()
-        if not os.path.isdir(userInput):
-            print("Please enter a valid directory:")
-            userInput = ""
-    return userInput
-
-def getUserInputTF(prompt):
-    print(prompt+" (Y/N)")
-    userInput = ""
-    while not userInput:
-        userInput = input()
-        if lower(userInput) != "y" and lower(userInput) != "n":
-            userInput = ""
-    if lower(userInput) == "y":
-        return True
-    else:
-        return False
 
 def getAllUserInput():
-    dataSource = getUserInputFile("Enter path to data input directory: ")
-    showPlots = getUserInputTF("Do you want to display the output plots?")
-    saveData = getUserInputTF("Do you want to save the output plots?")
-    if saveData:
-        savePath = getUserInputFile("Enter path to data output directory: ")
-    else:
-        savePath = "NA"
-
-    # Print results to inform user and begin program
-    # Could eventually add a "verbose" option into user input that regulates print() commands
-    print("Running with the following parameters:")
-    print("Path to input data: "+dataSource+"/")
-    print("Display plots: "+str(showPlots))
-    print("Save data: "+str(saveData))
-    print("Path to output data: "+savePath+"/\n")
+    dataSource = getUserInputFile("Enter path to data input directory:")
+    print("Enter plot title:")
+    plotTitle = input()
 
     # Build a dictionary to return values
     results = {
         'dataSource': dataSource,
-        'showPlots': showPlots,
-        'saveData': saveData
+        'title': plotTitle
     }
-    if saveData:
-        results.update( {'savePath': savePath })
 
     return results
 
@@ -121,7 +83,7 @@ blue_line = mlines.Line2D([], [], color='blue', label='Radiosonde flight')
 red_arrow = mlines.Line2D([], [], color='w', marker=r'$\rightarrow$', markeredgecolor='red', markerfacecolor='red', markersize=15, label='Gravity Wave')
 plt.legend(handles=[blue_line, red_arrow])
 
-plt.title("Week 7 Radiosonde Flight")
+plt.title(userInput.get('title'))
 plt.xlabel("Time [UTC]")
 plt.ylabel("Altitude [km]")
 plt.show()
